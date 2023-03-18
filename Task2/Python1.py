1bed9676bfc95cd380d759d8b50682efea4d63b3
Hassan <hassanali898768@gmail.com>
2023-03-18 20:02:59 +0500

    Added Python

---
+++ Task2/Python1.py
@@ -0,0 +1,23 @@
+print("Hello world")
+
+message = "Hello my name is hassan"
+print(message)
+
+name = "Hassan"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "Hassan"
+last_name = "ALi"
+Full_name = first_name +" "+last_name +"!"
+print(Full_name)
+
+
+print("Languages \n Python \n C++ \n\t Java")
+
+print(4**5)
+print((2+3)*4)
+print(0.9 + 0.10)
+
+#Hello This is Comment
