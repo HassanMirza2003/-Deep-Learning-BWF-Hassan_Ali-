75212896c167e0b6813a6e63aabb68982ee57662
Hassan <hassanali898768@gmail.com>
2023-03-21 17:23:50 +0500

    Added Task5

---
+++ Task5/UserInput+Conditions.py
@@ -0,0 +1,50 @@
+
+car = input("Enter the Name of The Car :")
+print("Let me Check if " +car +" is Available")
+
+people = input("How Many people? :")
+people = int(people)
+if people>8 :
+    print("You will have to wait\n")
+else:
+    print("The Table is ready")
+
+number = int(input("Enter a number: "))
+if number % 10 == 0:
+    print(number, "is a multiple of 10.")
+else:
+    print(number, "is not a multiple of 10.")
+
+
+alien_color = 'green'
+
+# if statement to check alien color
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
+
+alien_color = 'red'
+
+# if statement to check alien color
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
+
+alien_color = 'green'
+
+# if-else chain to check alien color
+if alien_color == 'green':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
+
+
+alien_color = 'red'
+
+# if-else chain to check alien color
+if alien_color == 'green':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
+
+
