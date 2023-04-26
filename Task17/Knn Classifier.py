84ee040c0256a96ce4dbbf8a1896b3cb4b570f50
Hassan <hassanali898768@gmail.com>
2023-04-26 19:15:12 +0500

    Added Task 17

---
+++ Task17/Knn Classifier.py
@@ -0,0 +1,20 @@
+import numpy as np 
+import pandas as pd 
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[: , :-1].values
+Y = dataset.iloc[: , -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5 , metric= 'minkowski' , p=2)
+classifier.fit(X_train, y_train)
+
+print(classifier.predict(sc.transform([[30,87000]])))
